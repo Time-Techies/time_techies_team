a1bbfa74d017c74a6a2afcaa050fe15fc0f66f03
SylsWorld <sylvermoones.world@gmail.com>
2024-11-06 12:58:58 -0600

    Updated main.tscn and Player.cs

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,20 +4,24 @@ namespace TimeTechiesGame.scripts;
 
 public partial class Player : CharacterBody2D
 {
+	[Export]
 	public const float Speed = 300.0f;
+	
+	[Export]
 	public const float JumpVelocity = -400.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
+			var gravity = GetGravity() * (float)delta;
+			Velocity += gravity;
 		}
 
 		// Handle Jump.
+		var velocity = new Vector2(Velocity.X, Velocity.Y);
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
@@ -25,10 +29,10 @@ public partial class Player : CharacterBody2D
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		var direction = Input.GetAxis("ui_left", "ui_right");
+		if (direction != 0)
 		{
-			velocity.X = direction.X * Speed;
+			velocity.X = direction * Speed;
 		}
 		else
 		{
